7113e1e8653dc702556160f129baff307bdb7117
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2021-04-27 10:13:07 -0600

    add DifferentialEquations test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,7 @@
-using GXBeam, Test, LinearAlgebra
+using GXBeam
+using LinearAlgebra
+using DifferentialEquations
+using Test
 import Elliptic
 
 @testset "Linear Analysis of a Cantilever Partially Under a Uniform Distributed Load" begin
@@ -932,4 +935,68 @@ end
         prescribed_conditions=prescribed_conditions)
 
     @test converged
+end
+
+@testset "DifferentialEquations" begin
+
+    L = 60 # m
+
+    # create points
+    nelem = 10
+    x = range(0, L, length=nelem+1)
+    y = zero(x)
+    z = zero(x)
+    points = [[x[i],y[i],z[i]] for i = 1:length(x)]
+
+    # index of endpoints of each beam element
+    start = 1:nelem
+    stop = 2:nelem+1
+
+    # stiffness matrix for each beam element
+    stiffness = fill(
+        [2.389e9  1.524e6  6.734e6 -3.382e7 -2.627e7 -4.736e8
+         1.524e6  4.334e8 -3.741e6 -2.935e5  1.527e7  3.835e5
+         6.734e6 -3.741e6  2.743e7 -4.592e5 -6.869e5 -4.742e6
+        -3.382e7 -2.935e5 -4.592e5  2.167e7 -6.279e5  1.430e6
+        -2.627e7  1.527e7 -6.869e5 -6.279e5  1.970e7  1.209e7
+        -4.736e8  3.835e5 -4.742e6  1.430e6  1.209e7  4.406e8],
+        nelem)
+
+    # mass matrix for each beam element
+    mass = fill(
+        [258.053      0.0        0.0      0.0      7.07839  -71.6871
+           0.0      258.053      0.0     -7.07839  0.0        0.0
+           0.0        0.0      258.053   71.6871   0.0        0.0
+           0.0       -7.07839   71.6871  48.59     0.0        0.0
+           7.07839    0.0        0.0      0.0      2.172      0.0
+         -71.6871     0.0        0.0      0.0      0.0       46.418],
+         nelem)
+
+    # create assembly of interconnected nonlinear beams
+    assembly = Assembly(points, start, stop; stiffness=stiffness, mass=mass)
+
+    # prescribed conditions
+    prescribed_conditions = (t) -> begin
+        Dict(
+            # fixed left side
+            1 => PrescribedConditions(ux=0, uy=0, uz=0, theta_x=0, theta_y=0, theta_z=0),
+            # force on right side
+            nelem+1 => PrescribedConditions(Fz=1e5*sin(20*t))
+            )
+    end
+
+    # define simulation time
+    tspan = (0.0, 2.0)
+
+    # run initial condition analysis to get consistent set of initial conditions
+    system, converged = initial_condition_analysis(assembly, tspan[1]; prescribed_conditions)
+
+    # construct DAEProblem
+    prob = DAEProblem(system, assembly, tspan; prescribed_conditions)
+
+    # solve DAEProblem
+    sol = solve(prob, DABDF2())
+
+    # test that solution worked
+    @test sol.t[end] == 2.0
 end
