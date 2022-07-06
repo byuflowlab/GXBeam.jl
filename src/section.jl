43e37ce7695db64c31543de99abc14b4547bb12a
Andrew Ning <aning@byu.edu>
2022-07-06 10:12:44 -0600

    rename some structs to prevent conflictgs with GXBeam, change ordering, allow plotting

--- src/section.jl
+++ src/section.jl
@@ -1,6 +1,5 @@
 using LinearAlgebra: I, Symmetric, det, factorize
 using SparseArrays: spzeros, sparse
-using StaticArrays
 
 """
     Material(E1, E2, E2, G12, G13, G23, nu12, nu13, nu23, rho)
@@ -48,7 +47,7 @@ struct Node{TF}
 end
 
 """
-    Element(nodenum, material, theta)
+    MeshElement(nodenum, material, theta)
 
 An element in the mesh, consisting of four ordered nodes, a material, and a fiber orientation.
 
@@ -58,7 +57,7 @@ An element in the mesh, consisting of four ordered nodes, a material, and a fibe
 - `material::Material`: material properties of this element
 - `theta::float`: fiber orientation
 """
-struct Element{VI, TF}
+struct MeshElement{VI, TF}
     nodenum::VI
     material::Material
     theta::TF
@@ -67,7 +66,7 @@ end
 """
 internal cache so allocations happen only once upfront
 """
-struct Cache{TM, TSM, TFM, TV}  # matrix, sparse matrix, float matrix, vector
+struct SectionCache{TM, TSM, TFM, TV}  # matrix, sparse matrix, float matrix, vector
     Q::TM
     Ttheta::TSM
     Tbeta::TSM
@@ -97,7 +96,9 @@ struct Cache{TM, TSM, TFM, TV}  # matrix, sparse matrix, float matrix, vector
 end
 
 """
-create chace.  set sizes of static matrices, and set sparsity patterns for those that are fixed.
+    initializecache(nodes, elements, etype=Float64)
+
+create cache.  set sizes of static matrices, and set sparsity patterns for those that are fixed.
 """
 function initializecache(nodes, elements, etype=Float64)
 
@@ -239,7 +240,7 @@ function initializecache(nodes, elements, etype=Float64)
     C .= 0.0
     M .= 0.0
 
-    cache = Cache(Q, Ttheta, Tbeta, Z, S, N, SZ, SN, Bksi, Beta, dNM_dksi, dNM_deta, BN, Ae, Re, Ee, Ce, Le, Me, idx, A, R, E, C, L, M)
+    cache = SectionCache(Q, Ttheta, Tbeta, Z, S, N, SZ, SN, Bksi, Beta, dNM_dksi, dNM_deta, BN, Ae, Re, Ee, Ce, Le, Me, idx, A, R, E, C, L, M)
 
     return cache
 end
@@ -524,16 +525,26 @@ function node2idx(nodenums)
 end
 
 
+"""
+Reorder stiffness or compliance matrix from internal order to GXBeam order
+"""
+function reorder(K)  # reorder to GXBeam format  (TODO: build this in and update tests)
+    idx = [3, 1, 2, 6, 4, 5]
+    return K[idx, idx]
+end
 
 
 """
-    compliance(nodes, elements)
+    compliance(nodes, elements; cache=initializecache(nodes, elements), gxbeam_order=true)
 
 Compute compliance matrix given the finite element mesh described by nodes and elements.
 
 **Arguments**
 - `nodes::Vector{Node}`: all the nodes in the mesh
-- `elements::Vector{Element}`: all the elements in the mesh
+- `elements::Vector{MeshElement}`: all the elements in the mesh
+- `gxbeam_order::Bool`: true if output compliance matrix should be in GXBeam order or internal ordering (see documentation)
+- `cache::SectionCache`: if number of nodes, number of elements, and connectivity of mesh stays the same (and you will be repeating calls)
+# then you can should initialize cache yourself and pass in so you don't have to keep reconstructing it.
 
 **Returns**
 - `S::Matrix`: compliance matrix
@@ -542,7 +553,7 @@ Compute compliance matrix given the finite element mesh described by nodes and e
 - `tc::Vector{float}`: x, y location of tension center, aka elastic center, aka centroid 
     (location where an axial force will not produce any bending, i.e., beam will remain straight)
 """
-function compliance(nodes, elements, cache=initializecache(nodes, elements))
+function compliance(nodes, elements; cache=initializecache(nodes, elements), gxbeam_order=true)
 
     # initialize
     ne = length(elements) # number of elements
@@ -619,11 +630,6 @@ function compliance(nodes, elements, cache=initializecache(nodes, elements))
     XY = [X; dX; Y]
     S = XY'*[E C R; C' M L; R' L' A]*XY
 
-    # S += 2*X'*L*dY  # NOTE: add additional term from 2015 paper, though I don't see how that follows...
-
-    # # stiffness matrix
-    # K = inv(Symmetric(S))
-
     xs = -S[6, 2]/S[6, 6]
     ys = S[6, 1]/S[6, 6]
     xt = (S[4, 4]*S[5, 3] - S[4, 5]*S[4, 3])/(S[4, 4]*S[5, 5] - S[4, 5]^2)
@@ -631,45 +637,22 @@ function compliance(nodes, elements, cache=initializecache(nodes, elements))
     sc = [xs, ys]
     tc = [xt, yt]
 
-    # K11 = [E R D; R' A zeros(6, 6); DT zeros(6, 12)]
-    # K12 = [C'-C  -L zeros(ndof, 6);
-    #         L' zeros(6, 12);
-    #         zeros(6, ndof+12)]
-    # W = [K11 K12; zeros(size(K11)) K11] \ [Bsub2; B2]
-    # G11 = [E R zeros(ndof, 6); R' A zeros(6, 6); zeros(6, ndof+12)]
-    # G12 = [C L zeros(ndof, 6); L' zeros(6, 12); zeros(6, ndof+12)]
-    # G22 = [M zeros(ndof, 12); zeros(12, ndof+12)]
-    # G = [G11 G12; G12' G22]
-    # Fs = W'*G*W
+
+    if gxbeam_order
+        S = reorder(S)
+    end
 
     return S, sc, tc
 end
 
-# function centers(S)  #, z, L)
-#     # assume free end
-#     # xs = -(S[6, 2] + S[6, 4]*(L - z))/S[6, 6]
-#     # ys = (S[6, 1] + S[6, 5]*(L - z))/S[6, 6]
-#     xs = -S[6, 2]/S[6, 6]
-#     ys = S[6, 1]/S[6, 6]
-#     xt = (S[4, 4]*S[5, 3] - S[4, 5]*S[4, 3])/(S[4, 4]*S[5, 5] - S[4, 5]^2)
-#     yt = (-S[4, 3]*S[5, 5] + S[4, 5]*S[5, 3])/(S[4, 4]*S[5, 5] - S[4, 5]^2) 
 
-#     return xs, ys, xt, yt
-# end
 
 """
-Reorder stiffness or compliance matrix from internal order to GXBeam order
+plot nodes and elements for a quick visualization
 """
-function reorder(K)  # reorder to GXBeam format
-    idx = [3, 1, 2, 6, 4, 5]
-    return K[idx, idx]
-end
-
-
-function plotmesh(nodes, elements; plotnumbers=false)
+function plotmesh(nodes, elements, pyplot; plotnumbers=false)
     ne = length(elements)
     
-    figure()
     for i = 1:ne
         node = nodes[elements[i].nodenum]
         for i = 1:4
@@ -677,529 +660,21 @@ function plotmesh(nodes, elements; plotnumbers=false)
             if iplus == 5
                 iplus = 1 
             end
-            plot([node[i].x, node[iplus].x], [node[i].y, node[iplus].y])
+            pyplot.plot([node[i].x, node[iplus].x], [node[i].y, node[iplus].y], "k")
         end
         if plotnumbers
             barx = sum([n.x/4 for n in node])
             bary = sum([n.y/4 for n in node])
-            text(barx, bary, string(i), color="r")
+            pyplot.text(barx, bary, string(i), color="r")
         end
     end
     if plotnumbers
         nn = length(nodes)
         for i = 1:nn
-            text(nodes[i].x, nodes[i].y, string(i))
+            pyplot.text(nodes[i].x, nodes[i].y, string(i))
         end
     end
 end
 
 
-
-# --------- Unit Tests --------
-using Test
-
-E1 = rand()
-E2 = rand()
-E3 = rand()
-G12 = rand()
-G13 = rand()
-G23 = rand()
-nu12 = rand()
-nu23 = rand()
-nu13 = rand()
-rho = 1.0
-mat = Material(E1, E2, E3, G12, G13, G23, nu12, nu13, nu23, rho)
-cache = initializecache([Node(0.0, 0.0), Node(0.0, 0.0), Node(0.0, 0.0), Node(0.0, 0.0)], [Element([1, 2, 3, 4], mat, 0.0)])
-stiffness!(mat, cache)
-Q1 = cache.Q
-
-nu21 = nu12*E2/E1
-nu31 = nu13*E3/E1
-nu32 = nu23*E3/E2
-C2 = [1/E2 -nu23/E2 0.0 0 0 -nu12/E1;
-       -nu23/E2 1/E3 0 0 0 -nu13/E1;
-       0 0 1/G23 0 0 0;
-       0 0 0 1/G12 0 0
-       0 0 0 0 1/G13 0;
-       -nu12/E1 -nu13/E1 0 0 0 1/E1]
-Q2 = inv(C2)
-
-
-@test all(isapprox.(Q1, Q2, atol=1e-6))
-
-
-# ---- Case 1: Square cross section of isotropic material - S1
-iso1 = Material(100.0, 100.0, 100.0, 41.667, 41.667, 41.667, 0.2, 0.2, 0.2, 1.0)
-x = range(-0.05, 0.05, length=11)
-y = range(-0.05, 0.05, length=11)
-
-nodes = Vector{Node}(undef, 11*11)
-elements = Vector{Element}(undef, 10*10)
-
-let
-m = 1
-for i = 1:11
-    for j = 1:11
-        nodes[m] = Node(x[i], y[j])
-        m += 1
-    end
-end
-
-m = 1
-for i = 1:10
-    for j = 1:10
-        elements[m] = Element([11*(i-1)+j, 11*(i)+j, 11*(i)+j+1, 11*(i-1)+j+1], iso1, 0.0)
-        m += 1
-    end
-end
-
-end
-
-using PyPlot
-close("all"); pygui(true);
-
-# plotmesh(nodes, elements, plotnumbers=true)
-
-S, sc, tc = compliance(nodes, elements)
-K = inv(S)
-
-@test isapprox(K[1, 1], 3.4899e-1, atol=0.0001e-1)
-@test isapprox(K[2, 2], 3.4899e-1, atol=0.0001e-1)
-@test isapprox(K[3, 3], 1.0, atol=1e-4)
-@test isapprox(K[4, 4], 8.3384e-4, atol=0.0001e-4)
-@test isapprox(K[5, 5], 8.3384e-4, atol=0.0001e-4)
-@test isapprox(K[6, 6], 5.9084e-4, atol=0.0001e-4)
-
-@test isapprox(sc[1], 0.0, atol=1e-8)
-@test isapprox(sc[2], 0.0, atol=1e-8)
-@test isapprox(tc[1], 0.0, atol=1e-8)
-@test isapprox(tc[2], 0.0, atol=1e-8)
-
-# --------- Case 2 --------
-alpha = 1e1
-iso2 = Material(100.0/alpha, 100.0/alpha, 41.667/alpha, 0.2, 1.0)  # note error in user guide for nu
-
-let
-m = 1
-for i = 1:10
-    for j = 1:10
-        if i <= 5
-            elements[m] = Element([11*(i-1)+j, 11*(i)+j, 11*(i)+j+1, 11*(i-1)+j+1], iso1, 0.0)
-        else
-            elements[m] = Element([11*(i-1)+j, 11*(i)+j, 11*(i)+j+1, 11*(i-1)+j+1], iso2, 0.0)
-        end
-        m += 1
-    end
-end
-end
-
-
-S, sc, tc = compliance(nodes, elements)
-K = inv(S)
-
-@test isapprox(K[1, 1], 1.28e-1, atol=0.01e-1)
-@test isapprox(K[2, 2], 1.92e-1, atol=0.01e-1)
-@test isapprox(K[3, 3], 5.50e-1, atol=0.01e-1)
-@test isapprox(K[4, 4], 4.59e-4, atol=0.01e-4)
-@test isapprox(K[5, 5], 4.59e-4, atol=0.01e-4)
-@test isapprox(K[6, 6], 2.77e-4, atol=0.01e-4)
-@test isapprox(K[2, 6], -3.93e-3, atol=0.01e-3)
-@test isapprox(K[3, 5], 1.13e-2, atol=0.01e-2)
-
-# ------ case 3 -------
-ortho = Material(480.0, 120.0, 120.0, 60.0, 50.0, 60.0, 0.19, 0.26, 0.19, 1.0)
-
-theta = 0.0
-let
-    m = 1
-    for i = 1:10
-        for j = 1:10
-            elements[m] = Element([11*(i-1)+j, 11*(i)+j, 11*(i)+j+1, 11*(i-1)+j+1], ortho, theta)
-            m += 1
-        end
-    end
-end
-
-S, sc, tc = compliance(nodes, elements)
-K = inv(S)
-@test isapprox(K[1, 1], 5.039E-01, atol=0.001e-1)
-@test isapprox(K[2, 2], 4.201E-01, atol=0.001e-1)
-@test isapprox(K[3, 3], 4.800E+00, atol=0.001e0)
-@test isapprox(K[4, 4], 4.001E-03, atol=0.001e-3)
-@test isapprox(K[5, 5], 4.001E-03, atol=0.001e-3)
-@test isapprox(K[6, 6], 7.737E-04, atol=0.001e-4)
-
-theta = 22.5*pi/180
-let
-    m = 1
-    for i = 1:10
-        for j = 1:10
-            elements[m] = Element([11*(i-1)+j, 11*(i)+j, 11*(i)+j+1, 11*(i-1)+j+1], ortho, theta)
-            m += 1
-        end
-    end
-end
-
-S, sc, tc = compliance(nodes, elements)
-K = inv(S)
-@test isapprox(K[1, 1], 7.598E-01, atol=0.001e-1)
-@test isapprox(K[2, 2], 4.129E-01, atol=0.001e-1)
-@test isapprox(K[3, 3], 3.435E+00, atol=0.001e0)
-@test isapprox(K[4, 4], 2.489E-03, atol=0.001e-3)
-@test isapprox(K[5, 5], 2.274E-03, atol=0.001e-3)
-@test isapprox(K[6, 6], 9.499E-04, atol=0.001e-4)
-@test isapprox(K[1, 3], 7.387E-01, atol=0.001e-1)
-@test isapprox(K[4, 6], -4.613E-04, atol=0.001e-4)
-
-theta = 90*pi/180
-let
-    m = 1
-    for i = 1:10
-        for j = 1:10
-            elements[m] = Element([11*(i-1)+j, 11*(i)+j, 11*(i)+j+1, 11*(i-1)+j+1], ortho, theta)
-            m += 1
-        end
-    end
-end
-
-S, sc, tc = compliance(nodes, elements)
-K = inv(S)
-@test isapprox(K[1, 1], 5.0202E-01, atol=0.0001e-1)
-@test isapprox(K[2, 2], 5.0406E-01, atol=0.0001e-1)
-@test isapprox(K[3, 3], 1.2, atol=0.001e0)
-@test isapprox(K[4, 4], 1.0004E-03, atol=0.0001e-3)
-@test isapprox(K[5, 5], 1.0002E-03, atol=0.0001e-3)
-@test isapprox(K[6, 6], 8.5081E-04, atol=0.0001e-4)
-
-
-# --------- cylinder -------
-R = 0.1
-t = 0.01
-nr = 4
-nt = 120
-r = range(R - t, R, length=nr)
-theta = range(0, 2*pi, length=nt)
-
-nodes = Vector{Node}(undef, nr*(nt-1))
-elements = Vector{Element}(undef, (nr-1)*(nt-1))
-let 
-m = 1
-for i = 1:nt-1
-    for j = 1:nr
-        nodes[m] = Node(r[j]*cos(theta[i]), r[j]*sin(theta[i]))
-        m += 1
-    end
-end
-end
-
-thetam = 0.5* (theta[1:end-1] + theta[2:end])
-beta = thetam .- pi/2
-
-let
-n = 1
-for i = 1:nt-1
-    for j = 1:nr-1
-        if i == nt-1
-            ip = 0
-        else
-            ip = i
-        end
-
-        elements[n] = Element([nr*ip+j, nr*(i-1)+j, nr*(i-1)+j+1, nr*ip+j+1], iso1, 0.0)
-        n += 1
-    end
-end
-end
-
-# plotmesh(nodes, elements)
-
-S, sc, tc = compliance(nodes, elements)
-K = inv(S)
-
-@test isapprox(K[1, 1], 1.249E-01, atol=0.001e-1/2)
-@test isapprox(K[2, 2], 1.249E-01, atol=0.001e-1/2)
-@test isapprox(K[3, 3], 5.965E-01, atol=0.0015e-1)
-@test isapprox(K[4, 4], 2.697E-03, atol=0.002e-3)  # mesh discretization is not the same
-@test isapprox(K[5, 5], 2.697E-03, atol=0.002e-3)
-@test isapprox(K[6, 6], 2.248E-03, atol=0.001e-3)
-
-
-# ---- C2 ----
-
-R = 0.1
-t = 0.01
-nr = 4
-nt = 60
-r = range(R - t, R, length=nr)
-theta = range(pi/2, 3*pi/2, length=nt)
-
-nodes = Vector{Node}(undef, nr*nt)
-elements = Vector{Element}(undef, (nr-1)*(nt-1))
-let 
-m = 1
-for i = 1:nt
-    for j = 1:nr
-        nodes[m] = Node(r[j]*cos(theta[i]), r[j]*sin(theta[i]))
-        m += 1
-    end
-end
-end
-
-
-let
-n = 1
-for i = 1:nt-1
-    for j = 1:nr-1
-        elements[n] = Element([nr*i+j, nr*(i-1)+j, nr*(i-1)+j+1, nr*i+j+1], iso1, 0.0)
-        n += 1
-    end
-end
-end
-
-# plotmesh(nodes, elements)
-
-S, sc, tc = compliance(nodes, elements)
-K = inv(S)
-
-@test isapprox(K[1, 1], 4.964E-02, atol=0.002e-2)
-@test isapprox(K[2, 2], 6.244E-02, atol=0.002e-2)
-@test isapprox(K[3, 3], 2.982E-01, atol=0.002e-1)
-@test isapprox(K[4, 4], 1.349E-03, atol=0.001e-3)
-@test isapprox(K[5, 5], 1.349E-03, atol=0.001e-3)
-@test isapprox(K[6, 6], 9.120E-04, atol=0.003e-4)
-@test isapprox(K[3, 5], 1.805E-02, atol=0.001e-2)
-@test isapprox(K[2, 6], -7.529E-03, atol=0.003e-3) 
-
-@test isapprox(sc[1], -1.206E-01, atol=0.001e-1) 
-@test isapprox(sc[2], 0.0, atol=1e-6) 
-@test isapprox(tc[1], -6.051E-02, atol=0.002e-2) 
-@test isapprox(tc[2], 0.0, atol=1e-6) 
-
-
-# A critical assessment of computer tools for calculating composite wind turbine blade properties
-# ------ circular tube -------
-E = 73e9
-nu = 0.33
-G = E/(2*(1 + nu))
-tratio = 1.0/3
-R = 0.3
-t = tratio*2*R
-circmat = Material(E, E, G, nu, 1.0)
-
-nr = 20
-nt = 100
-r = range(R - t, R, length=nr)
-theta = range(0.0, 2*pi, length=nt)
-
-nodes = Vector{Node}(undef, nr*(nt-1))
-elements = Vector{Element}(undef, (nr-1)*(nt-1))
-let 
-m = 1
-for i = 1:nt-1
-    for j = 1:nr
-        nodes[m] = Node(r[j]*cos(theta[i]), r[j]*sin(theta[i]))
-        m += 1
-    end
-end
-
-n = 1
-for i = 1:nt-1
-    for j = 1:nr-1
-        if i == nt-1
-            ip = 0
-        else
-            ip = i
-        end
-
-        elements[n] = Element([nr*ip+j, nr*(i-1)+j, nr*(i-1)+j+1, nr*ip+j+1], circmat, 0.0)
-        n += 1
-    end
-end
-end
-
-# plotmesh(nodes, elements)
-
-S, sc, tc = compliance(nodes, elements)
-K = inv(S)
-K2 = reorder(K)
-
-@test isapprox(K2[1, 1], 1.835e10, rtol=0.001)
-@test isapprox(K2[2, 2], 4.682e9, rtol=0.001)
-@test isapprox(K2[3, 3], 4.682e9, rtol=0.001)
-@test isapprox(K2[4, 4], 3.519e8, rtol=0.03)
-@test isapprox(K2[5, 5], 4.587e8, rtol=0.002)
-@test isapprox(K2[6, 6], 4.587e8, rtol=0.002)
-
-#  --- Generalized Timoshenko Theory of the Variational Asymptotic Beam Sectional Analysis ----
-# ----- multi-layer composite pipe --------
-
-# E1 = 141.963e9
-# E2 = 9.79056e9
-# nu12 = 0.42
-# G12 = 59.9844e9
-E1 = 20.59e6
-E2 = 1.42e6
-nu12 = 0.42
-G12 = 0.87e6
-rho = 1.0
-pipemat = Material(E1, E2, G12, nu12, rho)
-
-nx = 50
-nt = 24
-nr = 20
-nodes = Vector{Node}(undef, (2*(nx-1) + 2*(nt-1))*nr)
-elements = Vector{Element}(undef, (2*(nx-1) + 2*(nt-1))*(nr-1))
-
-let
-    # x1 = -50.8e-3/2
-    # y1 = 7.62e-3
-    # x2 = 50.8e-3/2
-    # y2 = 25.4e-3/2
-    x1 = -1.0
-    y1 = 0.3
-    x2 = 1.0
-    y2 = 0.5
-    
-    x = range(x1, x2, length=nx)
-    y = range(y1, y2, length=nr)
-
-    m = 1
-    for i = 1:nx
-        for j = 1:nr
-            nodes[m] = Node(x[i], y[j])
-            m += 1
-        end
-    end
-
-    n = 1
-    for i = 1:nx-1
-        for j = 1:nr-1
-            if j <= (nr ÷ 2)
-                theta = 90*pi/180
-            else
-                theta = 0.0
-            end
-            elements[n] = Element([nr*(i-1)+j, nr*(i)+j, nr*(i)+j+1, nr*(i-1)+j+1], pipemat, theta)
-            n += 1
-        end
-    end
-
-    I = nx-1
-
-    r = y
-    theta = reverse(range(-pi/2, pi/2, length=nt))
-    for i = 2:nt
-        for j = 1:nr
-            nodes[m] = Node(x2 + r[j]*cos(theta[i]), r[j]*sin(theta[i]))
-            m += 1
-        end
-    end
-
-    for i = 1:nt-1
-        for j = 1:nr-1
-            if j <= (nr ÷ 2)
-                theta = 45*pi/180
-            else
-                theta = -45*pi/180
-            end
-            elements[n] = Element([nr*(I+i-1)+j, nr*(I+i)+j, nr*(I+i)+j+1, nr*(I+i-1)+j+1], pipemat, theta)
-            n += 1
-        end
-    end
-
-    I += nt-1
-
-    # x1 = 50.8e-3/2
-    # y1 = -7.62e-3
-    # x2 = -50.8e-3/2
-    # y2 = -25.4e-3/2
-    x1 = 1.0
-    y1 = -0.3
-    x2 = -1.0
-    y2 = -0.5
-
-    x = reverse(range(x2, x1, length=nx))
-    y = reverse(range(y2, y1, length=nr))
-
-    for i = 2:nx
-        for j = 1:nr
-            nodes[m] = Node(x[i], y[j])
-            m += 1
-        end
-    end
-
-    for i = 1:nx-1
-        for j = 1:nr-1
-            if j <= (nr ÷ 2)
-                theta = 90*pi/180
-            else
-                theta = 0.0
-            end
-            elements[n] = Element([nr*(I+i-1)+j, nr*(I+i)+j, nr*(I+i)+j+1, nr*(I+i-1)+j+1], pipemat, theta)
-            n += 1
-        end
-    end
-
-    I += nx-1
-
-    theta = reverse(range(pi/2, 3*pi/2, length=nt))
-    for i = 2:nt-1
-        for j = 1:nr
-            nodes[m] = Node(x2 + r[j]*cos(theta[i]), r[j]*sin(theta[i]))
-            m += 1
-        end
-    end
-
-    for i = 1:nt-1
-        for j = 1:nr-1
-            if i == nt-1
-                Ip = -i
-            else
-                Ip = I
-            end
-            if j <= (nr ÷ 2)
-                theta = 45*pi/180
-            else
-                theta = -45*pi/180
-            end
-            elements[n] = Element([nr*(I+i-1)+j, nr*(Ip+i)+j, nr*(Ip+i)+j+1, nr*(I+i-1)+j+1], pipemat, theta)
-            n += 1
-        end
-    end
-
-end
-
-# plotmesh(nodes, elements)
-
-S, sc, tc = compliance(nodes, elements)
-K = inv(S)
-K2 = reorder(K)
-
-
-# using BenchmarkTools
-# cache = initializecache(nodes, elements)
-# @btime compliance($nodes, $elements, $cache)
-# @profview compliance(nodes, elements, cache)
-
-
-
-@test isapprox(K2[1, 1], 1.03892e7, rtol=0.04)
-@test isapprox(K2[2, 2], 7.85310e5, rtol=0.01)
-@test isapprox(K2[3, 3], 3.29279e5, rtol=0.02)
-@test isapprox(K2[1, 4], 9.84575e4, rtol=0.12)
-@test isapprox(K2[2, 5], -8.21805e3, rtol=0.11)
-@test isapprox(K2[3, 6], -5.20981e4, rtol=0.21)   
-@test isapprox(K2[4, 4], 6.87275e5, rtol=0.01)
-@test isapprox(K2[5, 5], 1.88238e6, rtol=0.04)
-@test isapprox(K2[6, 6], 5.38987e6, rtol=0.03)
-
-# println("K11 = ", round((K2[1, 1]/1.03892e7 - 1)*100, digits=2), "%")
-# println("K22 = ", round((K2[2, 2]/7.85310e5 - 1)*100, digits=2), "%")
-# println("K33 = ", round((K2[3, 3]/3.29279e5 - 1)*100, digits=2), "%")
-# println("K14 = ", round((K2[1, 4]/9.84575e4 - 1)*100, digits=2), "%")
-# println("K25 = ", round((K2[2, 5]/-8.21805e3 - 1)*100, digits=2), "%")
-# println("K36 = ", round((K2[3, 6]/-5.20981e4 - 1)*100, digits=2), "%")
-# println("K44 = ", round((K2[4, 4]/6.87275e5 - 1)*100, digits=2), "%")
-# println("K55 = ", round((K2[5, 5]/1.88238e6 - 1)*100, digits=2), "%")
-# println("K66 = ", round((K2[6, 6]/5.38987e6 - 1)*100, digits=2), "%")
 
