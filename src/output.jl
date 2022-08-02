99bb99dd71ba983ca2e205426d448aa92101be7e
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2022-08-02 11:29:23 -0600

    fix AssemblyState

--- src/output.jl
+++ src/output.jl
@@ -102,8 +102,8 @@ function AssemblyState(system, assembly, x = system.x;
 
     elements = extract_element_states(system, assembly, x; prescribed_conditions)
 
-    return AssemblyState{eltype(system), typeof(body), typeof(points), typeof(elements)}
-        (body, points, elements)
+    return AssemblyState{eltype(system), typeof(body), typeof(points), typeof(elements)}(
+        body, points, elements)
 end
 
 """
