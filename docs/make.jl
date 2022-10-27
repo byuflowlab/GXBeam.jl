3e3e66690c2e875aae8b17f6073cf9e50ff96934
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2022-10-27 14:05:29 -0600

    add section for sensitivity analysis

--- docs/make.jl
+++ docs/make.jl
@@ -12,7 +12,7 @@ makedocs(;
     pages = [
         "Home" => "index.md",
         "Getting Started" => joinpath("examples", "guide.md"),
-        "Computing Stiffness and Mass Matrices" => joinpath("examples", "section.md"),
+        "Section Properties" => joinpath("examples", "section.md"),
         "Examples" => [
             joinpath("examples", "cantilever.md"),
             joinpath("examples", "overdetermined.md"),
@@ -26,7 +26,8 @@ makedocs(;
             joinpath("examples", "dynamic-joined-wing.md"),
             joinpath("examples", "vertical-axis-wind-turbine.md"),
         ],
-        "Using GXBeam with DifferentialEquations.jl" => joinpath("examples", "diffeq.md"),
+        "DifferentialEquations" => joinpath("examples", "diffeq.md"),
+        "Sensitivity Analysis" => joinpath("examples", "sensitivity.md"), 
         "API Reference" => joinpath("reference", "reference.md"),
     ],
     sitename = "GXBeam.jl",
