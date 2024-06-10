5fb5b28224fd86302dfe4477bc628ec088832ef2
Andrew Ning <aning@byu.edu>
2024-06-10 13:39:57 -0600

    moving over section code

---
+++ crosssection/src/GXBeamCS.jl
@@ -0,0 +1,22 @@
+module GXBeamCS
+
+import ForwardDiff
+using UnPack
+using SparseArrays
+
+# from section
+export Material, Node, MeshElement
+export initialize_cache, compliance_matrix, mass_matrix, plotmesh
+export strain_recovery, plotsoln, tsai_wu
+
+# from afmesh
+export Layer
+export afmesh
+
+# section properties
+include("section.jl")
+
+# airfoil meshing
+include("afmesh.jl")
+
+end
